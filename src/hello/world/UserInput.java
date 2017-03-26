da0f42ccd43326292010bb2e5a48922cb3baa472
paulolemus <paulolemus14@gmail.com>
2017-03-25 21:47:42 -1000

    Completed dumb UserInput class

---
+++ src/hello/world/UserInput.java
@@ -0,0 +1,13 @@
+package hello.world;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+    public static string getUserInput() {
+        
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
+
+}
