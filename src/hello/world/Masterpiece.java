fe9c0469bc8a4f305fe2d3c81091154f79e8d1c6
paulolemus <paulolemus14@gmail.com>
2017-03-25 22:17:20 -1000

    This is a note.. At this point I added a great piece of code

---
+++ src/hello/world/Masterpiece.java
@@ -0,0 +1,8 @@
+package hello.world;
+
+public class Masterpiece {
+
+    public static void masterpiece() {
+        System.out.println("This is the greatest code that has ever been written");
+    }
+}
