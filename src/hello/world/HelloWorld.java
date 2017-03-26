69995cec858c5da477d4fa47ad270eea8a228143
jlouie2 <jlouie2@my.hpu.edu>
2017-03-25 21:46:01 -1000

    this is stupid

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author Jocelyn
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String input = "hello world";
+        if(input.matches("HelloWorld")){
+            System.out.println("It matches :D");
+        }
+        else{
+            System.out.println("It does not match :(");
+        }
+        
+    }
+    
+}
